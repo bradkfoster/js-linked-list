cb484067eef08e9c1c2f65e2616bb804204ee682
Bradley <brad@Bradleys-MacBook-Pro.local>
2018-01-09 06:10:50 -1000

    finished

--- linkedList.js
+++ linkedList.js
@@ -3,6 +3,105 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
-function linkedListGenerator(){
+function linkedListGenerator() {
 
+  let head = null;
+  let tail = null;
+
+  function getHead() {
+    return head;
+  }
+
+  function getTail() {
+    return tail;
+  };
+
+
+  function add(value) {
+    let node = {
+      value: value,
+      next: null,
+    }
+    if (!head) {
+      head = node;
+      tail = node;
+    } else {
+      tail.next = node;
+      tail = node;
+    }
+    return node
+  };
+
+
+
+  function get(num) {
+    let currentNode = head;
+    let count = 0;
+    while (count < num) {
+      if (currentNode.next === null) {
+        return false;
+      }
+      count++;
+      currentNode = currentNode.next
+    }if(num === count){
+      
+      return currentNode
+    }else{
+      return false;
+    }
+  }
+
+
+  function remove(index) {
+
+    let currentNode = get(index)
+
+    let prevNode = get(index - 1);
+    if (!currentNode) {
+      return false;
+    } else if (index === 0) {
+      head = head.next;
+      if (head === null) {
+        tail = null;
+      }
+    } else if (currentNode.next === null) {
+      prevNode.next = null;
+      tail = prevNode;
+    } else(prevNode.next = currentNode.next)
+
+
+  }
+
+
+
+
+
+
+  function insert(newValue, index) {
+    let currentNode = get(index);
+    let prevNode = get(index - 1)
+   
+    let newNode = {
+      value: newValue,
+      next: currentNode,
+    }
+   
+    if (currentNode === false) {
+      return false;
+    }else if(index === 0){
+      head = newNode
+    }else{
+      prevNode.next=newNode;
+
+    }
+
+  }
+  return {
+    getHead: getHead,
+    getTail: getTail,
+    add: add,
+    remove: remove,
+    get: get,
+    insert: insert,
+  }
 }
